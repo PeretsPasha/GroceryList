32d16411dc9739345c3d9d3537b58f89665dbbe3
Iron <perts.pasha.pro@gmail.com>
2020-05-07 09:37:04 +0300

    Add sound of trash

--- app/src/main/java/com/example/grocery/MainActivity.java
+++ app/src/main/java/com/example/grocery/MainActivity.java
@@ -8,6 +8,7 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.content.ContentValues;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -20,11 +21,14 @@ public class MainActivity extends AppCompatActivity {
     private EditText mEditTextName;
     private TextView mTextViewAmount;
     private int mAmount = 0;
+    private MediaPlayer mediaPlayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mediaPlayer = MediaPlayer.create(this, R.raw.trash);
 
         GroceryDBHelper dbHelper = new GroceryDBHelper(this);
         mDatabase = dbHelper.getWritableDatabase();
@@ -80,6 +84,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public boolean onLongClick(View v) {
                 clearList();
+                mediaPlayer.start();
                 return true;
             }
         });
